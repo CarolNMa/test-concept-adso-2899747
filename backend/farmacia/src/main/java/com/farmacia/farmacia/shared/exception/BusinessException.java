ec1a583b41a2ca3fa0c516e27ceba6fc2690c1b4
Carol <carolmarentes31@gmail.com>
2025-09-18 16:45:14 -0500

    chore: remove-backend-and-architecture

--- backend/farmacia/src/main/java/com/farmacia/farmacia/shared/exception/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package com.farmacia.farmacia.shared.exception;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
--- backend/farmacia/src/main/java/com/farmacia/farmacia/user/repository/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.farmacia.farmacia.user.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.farmacia.farmacia.user.model.Rol;
-
-public interface RolRepository extends JpaRepository<Rol, Long> {
-}
